69e12f4b7adbe51499b3feace3e0ac31bae32ae0
loclink <loclink@163.com>
2023-07-29 16:49:08 +0800

    fix: 修改文档调整代码格式

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # js-utils-template
+
+## 概述：
+开箱即用的 javascript 工具库开发模板
+
+## 特征：
+- 
--- src/modules/count/index.ts
+++ src/modules/count/index.ts
@@ -1,11 +1,10 @@
 /**
- * 两数之和
+ *  计算两数之和
  * @group 计算方法
  * @param num1 第一个加数
  * @param num2 第二个加数
  * @returns
- *
- * ### 示例
+ * @example
  * ``` ts
  * countSum(10, 10) // => 20
  * ```
@@ -13,4 +12,3 @@
 export const countSum = (num1: number, num2: number) => {
   return num1 + num2;
 };
-
